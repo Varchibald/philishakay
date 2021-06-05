50e3b6391c7ae995644cf64fe79ba5c674f637e1
Varchibald <56364904+Varchibald@users.noreply.github.com>
2021-06-05 14:34:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,28 @@ function unroll() {
   for (var i = 0; i < totalInputs; i++) {
     if (inputs[i].style.display === "block") {
       inputs[i].style.display = "none";
-    } else {
+    }
+    else {
       inputs[i].style.display = "block";
     }
+  }
+}
+
+
+var modal = document.getElementById("imprintModal");
+var trigger = document.getElementById("imprintTrigger");
+var span = document.getElementsByClassName("close")[0];
+
+trigger.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }
